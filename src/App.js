a48beb6382f417f8b040696d2b2e067a95333455
Dimitar Gospodinov <wardren@gmail.com>
2023-02-02 16:52:10 +0000

    added logo and navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
-
+import Navigation from './components/routes/navigation/navigation.component';
 import Home from './components/routes/home/home.component';
+
+const Shop = () => {
+return <h1>Shop page</h1>;
+};
 
 const App = () => {
 
   
   return ( 
+
+
     <Routes>
-    <Route path='/home' element={<Home /> } />
+    <Route path='/' element={<Navigation />}>
+
+    <Route index element={<Home /> } />
+    <Route path='shop' element={<Shop /> } />
+    </Route>
     
 
     </Routes>
