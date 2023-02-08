85521a3bf72ab805038d036ae47562b828debdd5
Dimitar Gospodinov <wardren@gmail.com>
2023-02-08 13:24:26 +0000

    created authentication page alongsite sign in form

--- src/components/routes/authentication/authentication.component.jsx
+++ src/components/routes/authentication/authentication.component.jsx
@@ -2,11 +2,11 @@ import SignUpForm from "../../sign-up-form/sign-up-form.component";
 
 import SignInForm from "../../sign-in-form/sign-in-form.component";
 
+import "./authentication.styles.scss";
+
 const Authentication = () => {
   return (
-    <div>
-      <h1>sign in page</h1>
-
+    <div className="authentication-container">
       <SignInForm />
       <SignUpForm />
     </div>
